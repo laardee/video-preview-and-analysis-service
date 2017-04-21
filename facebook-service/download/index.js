d6b99d3b2ef4a04f9fe8e4fb7d17c59e40ac7ef5
Eetu Tuomala <eetu.tuomala@sc5.io>
2017-04-21 15:09:53 +0300

    pass buffer without saving to disk, increase timeouts

--- facebook-service/download/index.js
+++ facebook-service/download/index.js
@@ -2,15 +2,10 @@
 
 const AWS = require('aws-sdk');
 const fetch = require('node-fetch');
-const fs = require('fs-extra');
 const url = require('url');
 const path = require('path');
-const BbPromise = require('bluebird');
 
 const s3 = new AWS.S3();
-
-const writeFile = BbPromise.promisify(fs.writeFile);
-const ensureDir = BbPromise.promisify(fs.ensureDir);
 
 module.exports.handler = (event, context, callback) => {
   if (event.Records && event.Records[0].Sns) {
@@ -18,21 +13,15 @@ module.exports.handler = (event, context, callback) => {
     const message = JSON.parse(notification.Message);
 
     const { base } = path.parse(url.parse(message.url).pathname);
-    const directory = path.join('/', 'tmp', 'videos', message.id);
-    const file = path.join(directory, base);
     const key = `videos/${message.id}/${base}`;
 
-    return ensureDir(directory)
-      .then(() => fetch(message.url))
-      .then((res) => res.buffer())
-      .then((buffer) =>
-        writeFile(file, buffer))
-      // pass buffer forward?
-      .then(() =>
+    return fetch(message.url)
+      .then(res => res.buffer())
+      .then(buffer =>
         s3.putObject({
           Bucket: process.env.SOURCE_BUCKET,
           Key: key,
-          Body: fs.readFileSync(file),
+          Body: buffer,
           ContentType: 'video/mp4',
         }).promise())
       .then(() => callback(null, 'ok'));
